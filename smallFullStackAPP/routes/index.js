93e97e126928cc71c153983365dfd32d28499049
anoopg <anoop.goudar@imaginea.com>
2016-12-24 14:26:38 +0530

    commented out the view engine setup and using html instead jade

--- smallFullStackAPP/routes/index.js
+++ smallFullStackAPP/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('layout', { title: 'Express' });
+  res.sendFile(path.resolve('../smallFullStackAPP/client/views/home.html'));
 });
 
 module.exports = router;
